819d895f9f3af6bf81c3d149a8febff22f67d791
koshiro0322 <nkoshiro322@gmail.com>
2024-04-08 23:41:10 +0900

    commition

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,56 +1,56 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_unless_owner, only: [:edit, :update]
+
   def index
     @prototypes = Prototype.all
   end
 
   def new
-    @prototype =Prototype.new
+    @prototype = Prototype.new
   end
 
   def create
-
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
-
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
+  end
 
-  end
-  def edit
-    @prototype = Prototype.find(params[:id])
-  end
+  def edit; end
+
   def update
-    if prototype = Prototype.find(params[:id])
-      prototype.update(prototype_params)
-      redirect_to prototype_path
-    else render :edit
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
     end
   end
+
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
+    @prototype.destroy
     redirect_to root_path
   end
 
   private
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    redirect_to root_path unless @prototype.user == current_user
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-end
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
   end
 end
